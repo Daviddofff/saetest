b3cb250fff6dd90c246d5d09ff4fc08be03bcb0a
M4hias <lypox@gmx.at>
2020-09-14 16:06:57 +0200

    X angelegt

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    int x = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x= x+5;
     }
 }
